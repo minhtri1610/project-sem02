eff6951336938663e748efd097bd27e6ebc88003
Minh Tri <epsminhtri@gmail.com>
2023-06-20 17:00:14 +0700

    update

--- Dim_Fact/update_or_create_data.py
+++ Dim_Fact/update_or_create_data.py
@@ -66,6 +66,42 @@ def execute_to_table(dbcursor, target_table, row, type_action):
                 # print(insert_query)
                 dbcursor.execute(insert_query, (row[index['customer_id']], row[index['company_name']], row[index['contact_name']], row[index['address']], row[index['city']], row[index['region']], row[index['postal_code']], row[index['country']], ))
 
+        if target_table == 'dim_employees':
+            col_id = 'employee_id'
+            index = {'employee_id': 0, 'last_name': 1, 'first_name': 2, 'title': 3, 'city': 4, 'region': 5, 'country': 6, 'reports_to': 7}
+
+            # if type_action == 'update':
+            #     update_query = f'UPDATE {target_schema}.{target_table} SET company_name = %s, contact_name = %s, address = %s, city = %s, region = %s, postal_code = %s, country = %s WHERE {col_id} = %s'
+            #     dbcursor.execute(update_query, (row[index['company_name']], row[index['contact_name']], row[index['address']], row[index['city']], row[index['region']], row[index['postal_code']], row[index['country']], row[index['customer_id']]))
+            #
+            # elif type_action == 'insert':
+            #     insert_query = f'INSERT INTO {target_schema}.{target_table} (customer_id, company_name, contact_name, address, city, region, postal_code, country) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'
+            #     # print(insert_query)
+            #     dbcursor.execute(insert_query, (row[index['customer_id']], row[index['company_name']], row[index['contact_name']], row[index['address']], row[index['city']], row[index['region']], row[index['postal_code']], row[index['country']], ))
+
+
+        if target_table == 'dim_products':
+            col_id = 'product_id'
+
+
+
+
+
+
+
+
+
+            index = {'product_id': 0, 'product_name': 1, 'quantity_per_unit': 2, 'unit_price': 3, 'units_in_stock': 4, 'units_on_order': 5, 'reorder_level': 6, 'discontinued': 7}
+
+            # if type_action == 'update':
+            #     update_query = f'UPDATE {target_schema}.{target_table} SET company_name = %s, contact_name = %s, address = %s, city = %s, region = %s, postal_code = %s, country = %s WHERE {col_id} = %s'
+            #     dbcursor.execute(update_query, (row[index['company_name']], row[index['contact_name']], row[index['address']], row[index['city']], row[index['region']], row[index['postal_code']], row[index['country']], row[index['customer_id']]))
+            #
+            # elif type_action == 'insert':
+            #     insert_query = f'INSERT INTO {target_schema}.{target_table} (customer_id, company_name, contact_name, address, city, region, postal_code, country) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'
+            #     # print(insert_query)
+            #     dbcursor.execute(insert_query, (row[index['customer_id']], row[index['company_name']], row[index['contact_name']], row[index['address']], row[index['city']], row[index['region']], row[index['postal_code']], row[index['country']], ))
+
     except Exception as e:
         print('Error execute_to_table:' + str(e))
 
@@ -79,28 +115,62 @@ def import_data(table_name):
             update_or_create_db(columns_source, 'customers', columns_target, 'dim_customer', 'customer_id')
             print('insert or update table dim_customer successfully')
 
-        if table_name == 'dim_customer':
+        if table_name == 'dim_employees':
             # columns
-            columns_source = ('customer_id, company_name, contact_name, address, city, region, postal_code, country')
+            columns_source = ('employee_id, last_name, first_name, title, city, region, country, reports_to')
             columns_target = []
-            update_or_create_db(columns_source, 'customers', columns_target, 'dim_customer', 'customer_id')
-            print('insert or update table dim_customer successfully')
+            update_or_create_db(columns_source, 'employees', columns_target, 'dim_employees', 'employee_id')
+            print('insert or update table dim_employees successfully')
 
-        if table_name == 'dim_customer':
+        if table_name == 'dim_products':
             # columns
-            columns_source = ('customer_id, company_name, contact_name, address, city, region, postal_code, country')
+            columns_source = ('product_id, product_name, quantity_per_unit, unit_price, units_in_stock, units_on_order, reorder_level, discontinued')
             columns_target = []
-            update_or_create_db(columns_source, 'customers', columns_target, 'dim_customer', 'customer_id')
-            print('insert or update table dim_customer successfully')
+            update_or_create_db(columns_source, 'products', columns_target, 'dim_products', 'product_id')
+            print('insert or update table dim_products successfully')
 
-        # if table_name == '':
-        #     update_or_create_db(columns, source_table, target_table, col_id)
-        #
-        # if table_name == '':
-        #     update_or_create_db(columns, source_table, target_table, col_id)
-        #
-        # if table_name == '':
-        #     update_or_create_db(columns, source_table, target_table, col_id)
+        # if table_name == 'fact_orders':
+            # order_id
+            # customer_id
+            # product_id
+            # employee_id
+            # required_date
+            # shipped_date
+            # unit_price
+            # quantity
+            # discount
+            # freight
+            # cumulated_percentage
+            # cumulated_sales
+            # customer_sales
+            # customer_sales_group
+            # cumulated_percentage_region
+            # cumulated_sales_region
+            # region_group
+            # region_sales
+
+            # columns_source = (
+            #     'product_id, product_name, quantity_per_unit, unit_price, units_in_stock, units_on_order, reorder_level, discontinued')
+            # columns_target = []
+            # update_or_create_db(columns_source, 'products', columns_target, 'dim_products', 'product_id')
+            # print('insert or update table dim_products successfully')
+
+        # if table_name == 'dim_date':
+            # id
+            # order_id
+            # order_date
+            # year
+            # month
+            # day
+            # short_name
+            # quarter
+
+            # columns_source = (
+            #     'order_id, product_name, quantity_per_unit, unit_price, units_in_stock, units_on_order, reorder_level, discontinued')
+            # columns_target = []
+            # update_or_create_db(columns_source, 'products', columns_target, 'dim_products', 'product_id')
+            # print('insert or update table dim_products successfully')
+
 
     except Exception as e:
         print('Error import_data:' + str(e))
